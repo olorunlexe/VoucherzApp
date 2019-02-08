c85e70afdaae6b33f03e7540e4d3e7eea5112359
jeff-ofobrukweta <oghenerukevwejeff@gmail.com>
2019-02-08 16:49:07 +0100

    remove keycloak.json config file

--- src/Constants/Configuration.js
+++ src/Constants/Configuration.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
   baseURL: `http://localhost:63745/api/v1`,
   timeout: 1000,
-  // headers: {
-  //   Authorization: 'bearer '+token,
-  // }
+  headers: {
+    Authorization: 'bearer '+token,
+  }
 });
